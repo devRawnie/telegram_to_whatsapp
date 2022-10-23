b37a891388515602be5bc88b2a987a3f4fe15062
Rohit Sharma <rohit.userfacet@gmail.com>
2022-10-23 23:44:02 +0530

    add app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from whatsapp_handler import send_message
+import requests
+import re
+
+TELEGRAM_BOT_TOKEN  = "5604217550:AAF8mWa6YnsP_Kkshh6Oys3JwNXkHogZfiM"
+TELEGRAM_WEBHOOK_REGISTRATION_URL = "https://api.telegram.org/bot{token}/setWebhook?url={url}"
+
+def sanitize_body(text):
+    space_regex = re.compile("\s{4, }")
+    text = space_regex.sub("", text)
+    return text.replace("\n", ",,,,,,,,,,,")
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "GET":
+        return "Hello World"
+
+    if request.method == "POST":
+        response = request.get_json()
+        message_body = response.get("message", {}).get("text", None)
+        send_message(sanitize_body(message_body))
+
+    return 'Invalid Request'
+
+
+@app.route("/set-webhook", methods=['GET'])
+def set_webhook():
+    endpoint = request.args.get("endpoint", None)
+    if endpoint is None:
+        return "ERROR: `endpoint` parameter is required"
+
+    # Register webhook
+    REQUEST_URL = TELEGRAM_WEBHOOK_REGISTRATION_URL.format(token=TELEGRAM_BOT_TOKEN, url=endpoint)
+    response = requests.get(REQUEST_URL)
+
+    if not response.ok:
+        return "There was an error registering the URL: " + endpoint
+
+    return "Registered URL: " + endpoint
+
+
+if __name__ == '__main__':
+   app.run()
--- whatsapp_handler.py
+++ whatsapp_handler.py
@@ -1,22 +1,52 @@
 from requests import post
+from time import sleep
 
-WHATSAPP_TOKEN = ""
 WHATSAPP_PHONE_NUMBER_ID = "100999242818609"
 WHATSAPP_MESSAGING_ENDPOINT = f"https://graph.facebook.com/v15.0/{WHATSAPP_PHONE_NUMBER_ID}/messages"
-WHATSAPP_TARGET_PHONE_NUMBER = "919910795695"
+# WHATSAPP_TARGET_PHONE_NUMBER = "919910795695"
+WHATSAPP_TARGET_PHONE_NUMBER = "918700486627"
 WHATSAPP_MESSAGE_TEMPLATE_NAME = "telegram_message"
-WHATSAPP_AUTHORIZATION_TOKEN = "EAAJaEUE8gJ4BABpnGFvc1l5jkPiQOYhZAgZBBz7my8IVEwn3cTq5yfDBbVAnIyJUpbyZBDKPvtuZA0AKfBf40fGD1igAQMA5EuGlZAQhAocPZCTgtaZCfh4PZBZBqWzfu48IgqZB5mZCWHhetGaenvciWB1bD4AEUhoH8BK6EuZANf79fZB65vMsbGnZA1"
+WHATSAPP_AUTHORIZATION_TOKEN = "EAAJaEUE8gJ4BAFjRbACzxQoSZBWM5jAS2SCz2MuKnwDm0YdsJYSoNYzjX1V7M1IOhcliG9oTIChrycccruusdqNkq7WgXBdTFblnIrZAWpSeC1bZBp49qlgrxiQGZCchETzKchGdZBGt1fi0VgnmbbvNKmLEtjUWZCQR63tlbde6FSKyAaLMdc"
 
-WHATSAPP_BODY = {
-    "messaging_product": "whatsapp",
-    "to": WHATSAPP_TARGET_PHONE_NUMBER,
-    "type": "template",
-    "template": {
-        "name": WHATSAPP_MESSAGE_TEMPLATE_NAME,
-        "language": { "code": "en_US" }
+def get_whatsapp_message_body(message):
+    return  {
+        "messaging_product": "whatsapp",
+        "to": WHATSAPP_TARGET_PHONE_NUMBER,
+        "type": "template",
+        "template": {
+            "name": WHATSAPP_MESSAGE_TEMPLATE_NAME,
+            "language": { "code": "en_US" },
+            "components": [
+                {
+                    "type": "body",
+                    "parameters": [
+                        {
+                            "type": "text",
+                            "text": message
+                        }
+                    ]
+                }
+            ]
+        }
     }
-}
 
 
-def send_message(message=""):
-    post(url=WHATSAPP_MESSAGING_ENDPOINT, json=WHATSAPP_BODY, headers={"Authorization": f"Bearer {WHATSAPP_AUTHORIZATION_TOKEN}"})
+def send_message(message="<test-message>"):
+    WHATSAPP_BODY = get_whatsapp_message_body(message)
+
+    attempts = 5
+    while attempts > 0:
+        try:
+            response = post(url=WHATSAPP_MESSAGING_ENDPOINT, json=WHATSAPP_BODY, headers={"Authorization": f"Bearer {WHATSAPP_AUTHORIZATION_TOKEN}"})
+            print(response.json())
+
+            if not response.ok:
+                raise Exception("Response not ok")
+
+            break
+
+        except Exception as e:
+            print("Error: ", e)
+            attempts -= 1
+
+            sleep(2)
